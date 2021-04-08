f3731ec33e357dcd186984f289c4482c887c3590
Gourav Kumar <gourav.kumar91@outlook.com>
2021-04-09 00:00:40 +0530

    Initial commit of IOC project

--- MicroRabbitInfra.IoC/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MicroRabbitInfra.IoC
-{
-    public class Class1
-    {
-    }
-}
---
+++ MicroRabbitInfra.IoC/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MicroRabbitInfra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
