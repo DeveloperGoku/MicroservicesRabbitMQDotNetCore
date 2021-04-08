dab827e3da66481ceca85ba521fc599f47b2cb79
Gourav Kumar <gourav.kumar91@outlook.com>
2021-04-08 14:36:56 +0530

    Created the Domain Entities.

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    // event handler takes in any type of event and implements event handler
+   public interface IEventHandler <in TEvent> : IEventHandler where TEvent: Event
+    {
+        Task Handle(TEvent @event);
+    }
+    public interface IEventHandler
+    {
+
+    }
+
+}
